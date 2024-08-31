af24ec3e12d211e8686304c94b421ce4b5745ac3
skril <skril@skrils-macbook-air.tail6bb62.ts.net>
2024-08-31 17:41:43 +0300

    feat: fix tests

--- src/test/scala/dwayne/org/OrgParserSpec.scala
+++ src/test/scala/dwayne/org/OrgParserSpec.scala
@@ -1,15 +1,14 @@
 package dwayne.org
 
 import cats.syntax.all.*
-import dwayne.codec.ValidatedCodec
-import dwayne.org.Task
+import dwayne.org.OrgParser.taskTitleParser
 import dwayne.parser.*
 import zio.{Task => _, _}
 import zio.test._
 
 object OrgCodecSpec extends ZIOSpecDefault {
 
-  val titleLineParts: List[(Task => Boolean, String)] = List(
+  val titleLineParts: List[(OrgTaskTitle => Boolean, String)] = List(
     (t => t.state.map(_ == "TODO").getOrElse(false), "TODO"),
     (t => t.priority === 0.some, "[#A]"),
     (t => t.title === "Some title", "Some title"),
@@ -25,12 +24,13 @@ object OrgCodecSpec extends ZIOSpecDefault {
     }
 
   def makeTest(
-      l: List[(Task => Boolean, String)]
+      l: List[(OrgTaskTitle => Boolean, String)]
   ): Spec[Any, Nothing] = {
     val titleLine = f"* ${l.map(_._2).mkString(" ")}"
     test(s"Should parse correctly '$titleLine'") {
-      val parcedTask: ValidatedCodec[Task] = titleLine.decode
-      val task                             = parcedTask.toOption.get
+      val parcedTask: Either[InvalidSyntaxError | UnexpectedError, PResult[OrgTaskTitle]] =
+        taskTitleParser.parse(PInput(Text(titleLine), Location(0, 0)))
+      val task = parcedTask.toOption.get.result
       l.foldLeft(assertTrue(true)) { case (acc, (assertion, _)) =>
         acc && assertTrue(assertion(task))
       }
--- src/test/scala/dwayne/parser/ParserSpec.scala
+++ src/test/scala/dwayne/parser/ParserSpec.scala
@@ -6,21 +6,29 @@ import zio.{Task => _, _}
 import zio.test._
 
 object TransformersSpec extends ZIOSpecDefault {
+
   val digitParser = new Parser[Option, Int] {
-    def parse(in: PInput) =
-      in.text.headOption.flatMap { c =>
-        Try(Integer.parseInt(c.toString)).toOption.map(digit =>
-          PResult(digit, PInput(in.text.tail, in.location.modifyColumn(_ + 1)))
-        )
-      }
+    def parse(in: PInput): Option[PResult[Int]] =
+      in.text.lines match
+        case head :: next =>
+          head.headOption.flatMap { c =>
+            Try(Integer.parseInt(c.toString)).toOption.map(digit =>
+              PResult(digit, PInput(Text(List(head.tail).appendedAll(next)), in.location.modifyColumn(_ + 1)))
+            )
+          }
+        case Nil => None
   }
 
   def wordParser(str: String) = new Parser[Option, String] {
-    def parse(in: PInput): Option[PResult[String]] =
-      in.text.split(" ").toList match {
-        case str :: rest => Some(PResult(str, PInput(rest.mkString(" "), in.location)))
-        case Nil         => None
-      }
+    def parse(in: PInput): Option[PResult[String]] = in.text.lines match {
+      case head :: next =>
+        head.split(" ").toList match {
+          case str :: rest => Some(PResult(str, PInput(Text(rest.mkString(" ")), in.location)))
+          case Nil         => None
+        }
+      case Nil => None
+    }
+
   }
 
   def spec =
@@ -31,7 +39,7 @@ object TransformersSpec extends ZIOSpecDefault {
           _.isDefined,
           input.split(" ").map(wordParser).toList
         )
-        val res = parallel.parse(PInput(input, Location(0, 0))).value.flatten
+        val res = parallel.parse(PInput(Text(input), Location(0, 0))).value.flatten
 
         assertTrue(res.isDefined) &&
         assertTrue(res.get.result == "SCHEDULED")
@@ -44,8 +52,8 @@ object TransformersSpec extends ZIOSpecDefault {
           four  <- digitParser
         } yield List(one, two, three, four)
 
-        val input = "1234"
-        val parsed = manual.parse(PInput(input, Location(0, 0)))
+        val input  = "1234"
+        val parsed = manual.parse(PInput(Text(input), Location(0, 0)))
 
         assertTrue(parsed.isDefined) &&
         assertTrue(parsed.get.result == List(1, 2, 3, 4))
@@ -58,15 +66,15 @@ object TransformersSpec extends ZIOSpecDefault {
           four  <- digitParser
         } yield List(one, two, three, four)
 
-        val input = "1a34"
-        val parsed = manual.parse(PInput(input, Location(0, 0)))
+        val input  = "1a34"
+        val parsed = manual.parse(PInput(Text(input), Location(0, 0)))
 
         assertTrue(parsed.isEmpty)
       },
       test("repeatUntil should work correctly") {
-        val l = (1 to 9).toList
+        val l        = (1 to 9).toList
         val repeated = digitParser.repeatedUntil(_.isEmpty)
-        val res = repeated.parse(PInput(l.mkString, Location(0, 0)))
+        val res      = repeated.parse(PInput(Text(l.mkString), Location(0, 0)))
 
         assertTrue(res.isDefined) &&
         assertTrue(res.get.result == l)
